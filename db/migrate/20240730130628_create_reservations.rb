5e7bd7c8cb16a3d38ed968b711c99a9bf3637ae2
Thuthikaran Easvaran <thuthikaraneasvaran@gmail.com>
2024-07-30 15:06:41 +0200

    generated the reservation model

---
+++ db/migrate/20240730130628_create_reservations.rb
@@ -0,0 +1,15 @@
+class CreateReservations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reservations do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :car, null: false, foreign_key: true
+      t.date :start_date
+      t.date :end_date
+      t.decimal :total_price
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps
+    end
+  end
+end
