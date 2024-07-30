4bd0ea909639300c390e27811a6b3f5aa8bfe2ce
Thuthikaran Easvaran <thuthikaraneasvaran@gmail.com>
2024-07-30 15:36:27 +0200

    created the first car using the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+
+# Find the user with ID 1 and assign it to a variable named 'thuthi'
+thuthi = User.find(1)
+
+# Create a car associated with this user
+Car.create!(
+  user: thuthi,
+  brand: 'Toyota',
+  model: 'Camry',
+  year_of_production: 2020,
+  address: '123 Main St',
+  price_per_day: 30.00,
+  color: 'Blue',
+  image_urls: 'No image available',  # Placeholder text
+  transmission: 'Automatic',
+  mileage: 15000,
+  seats: 5,
+  fuel_type: 'Gasoline'
+)
+
+puts "Created a car for user #{thuthi.email}"
