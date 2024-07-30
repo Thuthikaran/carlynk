46ac2bfe66c1efffcf1cbd4706bd8a95b1db1084
Thuthikaran Easvaran <thuthikaraneasvaran@gmail.com>
2024-07-30 15:06:15 +0200

    Generated the car model

---
+++ db/migrate/20240730130600_create_cars.rb
@@ -0,0 +1,22 @@
+class CreateCars < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cars do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :brand
+      t.string :model
+      t.integer :year_of_production
+      t.string :address
+      t.decimal :price_per_day
+      t.string :color
+      t.text :image_urls
+      t.string :transmission
+      t.integer :mileage
+      t.integer :seats
+      t.string :fuel_type
+      t.datetime :updated_at
+      t.datetime :created_at
+
+      t.timestamps
+    end
+  end
+end
